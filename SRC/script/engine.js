b80cdcd24c8e9d33eadc3718dc541e001bdbd2ab
Olivia <seuemail@exemplo.com>
2024-11-04 21:36:39 -0400

    Primeiro commit

---
+++ SRC/script/engine.js
@@ -0,0 +1,68 @@
+const state = {
+    view:{
+
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+
+    },
+    values:{
+        timerId: null,
+        countDownTimerId: setInterval(countDown,1500),
+        gameVelocity:1500,
+        hitPositon: 0,
+        result: 0,
+        curretTime: 60,
+    },
+};
+
+    function countDown(){
+        state.values.curretTime--;
+        state.view.timeLeft.textContent = state.values.curretTime;
+
+        if (state.values.curretTime < 0){
+            clearInterval(state.actions.countDownTimerId);
+            clearInterval(state.actions.TimerId);   
+            alert("Game Over! O seu resultado foi: " + state.values.result);   
+        }
+    }
+
+    function playSound(){
+        let audio = new Audio("./SRC/audios/AUDIO..mp3");
+        audio.play();
+    }
+    
+    function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPositon = randomSquare.id;
+}
+    function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+    }
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPositon){
+            state.values.result++;
+            state.view.score.textContent = state.values.result;
+            state.values.hitPositon = null;
+            playSound();
+            }
+        });
+    });
+}
+
+function initialize(){
+    moveEnemy();
+    addListenerHitBox();
+}
+
+initialize();
